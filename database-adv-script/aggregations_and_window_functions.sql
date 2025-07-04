8f352bcba6ca2ba3fe0c09e430bb424401a3446f
Peter Mburu <peter.mburu@kimetrica.com>
2025-07-05 00:45:21 +0300

    Apply Aggregations and Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,13 +9,20 @@ LEFT JOIN bookings b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
--- 2. Rank properties based on the total number of bookings they have received
+-- 2. Rank properties based on the total number of bookings they have received using ROW_NUMBER and RANK
 SELECT
-    p.property_id,
-    p.name AS property_name,
-    COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
-FROM properties p
-LEFT JOIN bookings b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.name
-ORDER BY booking_rank;
+    property_id,
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number_rank,
+    RANK() OVER (ORDER BY total_bookings DESC) AS dense_rank
+FROM (
+    SELECT
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM properties p
+    LEFT JOIN bookings b ON p.property_id = b.property_id
+    GROUP BY p.property_id, p.name
+) AS ranked_properties;
+
